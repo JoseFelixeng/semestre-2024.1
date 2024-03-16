404c7f72203f75104805f20dcb9fc44046bbc22c
JoseFelixeng <jfelixnunes2k15@gmail.com>
2024-03-16 20:27:43 -0300

    adcionando completos a aula 3

--- Algoritmo e Estrutura de Dados I/1 Unidade/list/array_list.go
+++ Algoritmo e Estrutura de Dados I/1 Unidade/list/array_list.go
@@ -37,3 +37,36 @@ func (list *ArrayList) doubleArray() {
 	// Adiciona o tamanho duplicado a lista
 	list.values = doubledValues
 }
+
+/**
+1. Adicionar sempre da esquerda para a direita, após o ultimo elemento inserido anteriormente.
+2. Melhor Caso: ômega(1)
+Just: não precisa duplicar o vetor, nem varrer o vetor do início para o endereço a ser Add
+3. Pior caso: O(n)
+Just: Duplicar o vetor requer copiar os elementos para o novo vetor, causando um custo computacional
+proporcional ao tamanho do vetor (n).
+
+**/
+
+// Função para adicionar elemento
+func (list *ArrayList) Add(val int) {
+	// Verificar se array está lotado
+	if list.inserted >= len(list.values) {
+		list.doubleArray()
+	}
+	// Após verificar, identifica a posição e inseri o novo item
+	list.values[list.inserted] = val
+	list.inserted++
+}
+
+/**
+1. Adiciona elemento na posição especificada, deslocando os elementos à direita dessa posição
+2. Melhor caso: ômega(1)
+just: Não precisa duplicar o vetor, nem varrer o vetor do ínicio para encontrar o endereço a ser ADD.
+3. Pior caso: O(n):
+Just:
+	1) Duplicar o vetor requer copiar os elementos para o novo vetor, causando um custo computacional proporcional
+	ao tamanho do vetor(n);
+	2) Adicionar no início requer deslocar os n elementos para a direita
+
+**/
